590aa250e43ce0a90ae83cf16ae2917b79447679
Walter Coayla <walter.coayla@gmail.com>
2024-10-16 08:48:57 -0500

    Act. 161024

--- views/index.php
+++ views/index.php
@@ -77,7 +77,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    
     <!-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
